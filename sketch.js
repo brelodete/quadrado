b167ead5e95e04f9a8a9bd595aab1b1f16b1bc9c
brelodete <00001116099780SP@al.educacao.sp.gov.br>
2024-06-04 19:55:27 -0300

    sketch.js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  // cria uma tela de 4000px de largura por 400px de altura
+
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("white");
+}
+  function draw() {
+  stroke("blue") 
+  fill("red");
+    
+  if (mouseIsPressed) {
+  rect(mouseX, mouseY, 20, 35);
+  }
+}
